c8318f8e3308ef0d103a7ee27b9fdb07ea3a63ac
jaraco <none@none>
2008-06-15 01:42:41 +0000

    Improved DictFilter with a pattern_include parameter

--- src/jaraco/util/dictlib.py
+++ src/jaraco/util/dictlib.py
@@ -3,6 +3,24 @@
 
 # $Id$
 
+import re, sets
+
+class NonDataProperty(object):
+	"""Much like the property builtin, but only implements __get__,
+	making it a non-data property, and can be subsequently reset.
+	
+	See http://users.rcn.com/python/download/Descriptor.htm for more
+	information."""
+	
+	def __init__(self, fget):
+		assert fget is not None, "fget cannot be none"
+		# todo, make sure fget is callable
+		self.fget = fget
+		
+	def __get__(self, obj, objtype=None):
+		if obj is None:
+			return self
+		return self.fget(obj)
 
 class DictFilter(object):
 	"""
@@ -12,10 +30,45 @@ class DictFilter(object):
 	>>> filtered = DictFilter(sample, ['a', 'c'])
 	>>> filtered == {'a': 1, 'c': 3}
 	True
+	
+	One can also filter by a regular expression pattern
+	
+	>>> sample['d'] = 4
+	>>> sample['ef'] = 5
+	
+	Here we filter for only single-character keys
+	
+	>>> filtered = DictFilter(sample, include_pattern='.$')
+	>>> filtered == {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+	True
+	
+	Also note that DictFilter keeps a reference to the original dict, so
+	if you modify the original dict, that could modify the filtered dict.
+	
+	>>> del sample['d']
+	>>> del sample['a']
+	>>> filtered == {'b': 2, 'c': 3}
+	True
+	
 	"""
-	def __init__(self, dict, include_keys):
+	def __init__(self, dict, include_keys=[], include_pattern=None):
 		self.dict = dict
-		self.include_keys = set(include_keys)
+		self.specified_keys = sets.Set(include_keys)
+		if include_pattern is not None:
+			self.include_pattern = re.compile(include_pattern)
+		else:
+			# for performance, replace the pattern_keys property
+			self.pattern_keys = sets.Set()
+
+	def get_pattern_keys(self):
+		#key_matches = lambda k, v: self.include_pattern.match(k)
+		keys = filter(self.include_pattern.match, self.dict.keys())
+		return sets.Set(keys)
+	pattern_keys = NonDataProperty(get_pattern_keys)
+
+	@property
+	def include_keys(self):
+		return self.specified_keys.union(self.pattern_keys)
 
 	def keys(self):
 		return self.include_keys.intersection(self.dict.keys())
