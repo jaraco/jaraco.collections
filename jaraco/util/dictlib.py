d6613af2bbcf7fdab2bff236be7bcd2500677570
Jason R. Coombs <jaraco@jaraco.com>
2014-10-21 14:32:26 +0200

    Update documentation for nicer formatting

--- jaraco/util/dictlib.py
+++ jaraco/util/dictlib.py
@@ -109,22 +109,27 @@ class RangeMap(dict):
 	to sort keys (i.e. cmp [python 2 only], keys, reverse) as sort_params.
 
 	Let's create a map that maps 1-3 -> 'a', 4-6 -> 'b'
+
 	>>> r = RangeMap({3: 'a', 6: 'b'})  # boy, that was easy
 	>>> r[1], r[2], r[3], r[4], r[5], r[6]
 	('a', 'a', 'a', 'b', 'b', 'b')
 
 	Even float values should work so long as the comparison operator
 	supports it.
+
 	>>> r[4.5]
 	'b'
 
-	But you'll notice that the way rangemap is defined, it must be open-ended on one side.
+	But you'll notice that the way rangemap is defined, it must be open-ended
+	on one side.
+
 	>>> r[0]
 	'a'
 	>>> r[-1]
 	'a'
 
 	One can close the open-end of the RangeMap by using undefined_value
+
 	>>> r = RangeMap({0: RangeMap.undefined_value, 3: 'a', 6: 'b'})
 	>>> r[0]
 	Traceback (most recent call last):
@@ -132,25 +137,28 @@ class RangeMap(dict):
 	KeyError: 0
 
 	One can get the first or last elements in the range by using RangeMap.Item
+
 	>>> last_item = RangeMap.Item(-1)
 	>>> r[last_item]
 	'b'
 
 	.last_item is a shortcut for Item(-1)
+
 	>>> r[RangeMap.last_item]
 	'b'
 
 	Sometimes it's useful to find the bounds for a RangeMap
+
 	>>> r.bounds()
 	(0, 6)
 
 	RangeMap supports .get(key, default)
+
 	>>> r.get(0, 'not found')
 	'not found'
 
 	>>> r.get(7, 'not found')
 	'not found'
-
 	"""
 	def __init__(self, source, sort_params = {}, key_match_comparator = operator.le):
 		dict.__init__(self, source)
