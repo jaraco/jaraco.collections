e3289431ea0c67c8117160a397cf5910462f8a39
Jason R. Coombs <jaraco@jaraco.com>
2019-10-13 11:54:38 -0400

    Fade to black

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -4,25 +4,26 @@ from jaraco import collections
 
 
 class AlwaysStringKeysDict(collections.KeyTransformingDict):
-	"""
-	An implementation of a KeyTransformingDict subclass that always converts
-	the keys to strings.
-	"""
-	@staticmethod
-	def transform_key(key):
-		return six.text_type(key)
+    """
+    An implementation of a KeyTransformingDict subclass that always converts
+    the keys to strings.
+    """
+
+    @staticmethod
+    def transform_key(key):
+        return six.text_type(key)
 
 
 def test_always_lower_keys_dict():
-	"""
-	Tests AlwaysLowerKeysDict to ensure KeyTransformingDict works.
-	"""
-	d = AlwaysStringKeysDict()
-	d['farther'] = 'closer'
-	d['Lasting'] = 'fleeting'
-	d[3] = 'three'
-	d[{'a': 1}] = 'a is one'
-	assert all(isinstance(key, six.text_type) for key in d)
-	assert "{'a': 1}" in d
-	assert 3 in d
-	assert d[3] == d['3'] == 'three'
+    """
+    Tests AlwaysLowerKeysDict to ensure KeyTransformingDict works.
+    """
+    d = AlwaysStringKeysDict()
+    d['farther'] = 'closer'
+    d['Lasting'] = 'fleeting'
+    d[3] = 'three'
+    d[{'a': 1}] = 'a is one'
+    assert all(isinstance(key, six.text_type) for key in d)
+    assert "{'a': 1}" in d
+    assert 3 in d
+    assert d[3] == d['3'] == 'three'
